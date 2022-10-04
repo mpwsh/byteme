f6e8da44b36230adfb411529f1129d74fa7c89cc
mpw <x@mpw.sh>
2022-10-04 20:32:06 -0300

    adding readme, updating non-working code heh

---
+++ readme.md
@@ -0,0 +1,42 @@
+### Description
+Compress/Decompress, Encode and Decode any file to a hex string.
+Useful to store binaries in text format, for NFC cards/stickers/keychains or wherever you want.
+
+## Build
+```bash
+#Build
+❯ cargo build --release
+```
+
+## Usage
+Compress a file and encode bytes to hex
+```bash
+#Create a test file
+echo "This is a test" >> raw.txt
+#Process
+❯ ./target/release/byteme raw.txt
+#Output
+78da0bc9c82c5600a2448592d4e2122e0029730500
+```
+
+Decode hex and decompress back to original file
+```bash
+#Decode
+❯ echo "78da0bc9c82c5600a2448592d4e2122e0029730500" | ./target/release/byteme out.txt
+#Check your new file
+❯ cat out.txt
+This is a test
+```
+
+
+
+## Encryption
+Encrypt a file using `gpg` and process with `byteme`
+```bash
+#Interactive mode
+❯ gpg -c --no-symkey-cache raw.txt
+#No interactive mode
+❯ gpg --batch --passphrase 'somepass' -c raw.txt
+```
+
+Now run the steps above but using `<filename>.gpg` instead.
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,57 @@
-use std::io::{Read, Write};
-use std::fs::File;
-use flate2::Compression;
-use flate2::write::ZlibEncoder;
-use flate2::read::ZlibDecoder;
 use anyhow::Result;
-use std::env;
-use std::path::Path;
-use std::io::{self, BufRead};
-use std::process;
 use core::str;
+use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::io::{Read, Write};
+use std::path::Path;
+use std::process;
 use std::{fmt::Write as fmtWrite, num::ParseIntError};
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
-    if args.get(1).is_none() && atty::is(atty::Stream::Stdin) {
-        println!("File will be compressed and will output a hex string on stdout");
-        println!("Usage:\nCompress: byteme <path>\nDecompress: echo '<your-hex-string> | byteme <path-to-save-decompressed-file>' ");
+    let tty = atty::is(atty::Stream::Stdin);
+    //Usage
+    if args.get(1).is_none() {
+        println!(
+            "
+        Provided path will be compressed and a hex string will be sent stdout.
+        To convert back to original file read instructions below.
+        ----------------
+        Usage:
+        Compress: byteme <path>
+        Decompress: echo '<your-hex-string> | byteme <path-to-save-output-file>'
+        ---------------
+        Hint: If you're seeing this message something went wrong.
+        "
+        );
         process::exit(0x0100);
+    } else {
+        match tty {
+            false => {
+                //Decode + Decompress + Save to file
+                let mut buffer = String::new();
+                let stdin = io::stdin();
+                let mut handle = stdin.lock();
+                handle.read_line(&mut buffer)?;
+                write_to_file(decompress(decode_hex(buffer.trim_end())?)?, &args[1])?;
+            }
+            true => {
+                //Read from path + Compress + Encode
+                match Path::new(&args[1]).exists() {
+                    true => println!("{}", encode_hex(&compress(read_from_file(&args[1])?)?)),
+                    false => println!("File doesn't exist. Check path"),
+                };
+                process::exit(0x0100);
+            }
+        }
     };
-    if args.get(1).is_some(){
 
-    match Path::new(&args[1]).exists() {
-    true => println!("{}", encode_hex(&compress(read_from_file(&args[1])?)?)),
-    false => println!("File doesn't exist. Check path"),
-    };
-        process::exit(0x0100);
-    }
-    let mut buffer = String::new();
-    let stdin = io::stdin();
-    let mut handle = stdin.lock();
-    handle.read_line(&mut buffer)?;
-    write_to_file(
-        decompress(decode_hex(buffer.trim_end())?)?
-        , "decompressed.file")?;
     Ok(())
 }
-
 
 pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
     (0..s.len())
@@ -63,13 +79,13 @@ fn read_from_file(path: &str) -> Result<Vec<u8>> {
     Ok(data)
 }
 
-fn compress(bytes: Vec<u8>) -> Result<Vec<u8>>{
+fn compress(bytes: Vec<u8>) -> Result<Vec<u8>> {
     let mut e = ZlibEncoder::new(Vec::new(), Compression::best());
     e.write_all(&bytes)?;
     let compressed_bytes = e.finish()?;
     Ok(compressed_bytes)
 }
-fn decompress(bytes: Vec<u8>) -> Result<Vec<u8>>{
+fn decompress(bytes: Vec<u8>) -> Result<Vec<u8>> {
     let mut d = ZlibDecoder::new(&*bytes);
     let mut b = Vec::new();
     d.read_to_end(&mut b)?;
