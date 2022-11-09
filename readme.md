2550eb3ace9b06d30cdd035f9320d08d2bf21ce7
mpw <x@mpw.sh>
2022-11-09 01:16:44 -0300

    using z85 encoding instead of hex to improve string size

--- readme.md
+++ readme.md
@@ -1,15 +1,19 @@
 ## Description
-Compress/Decompress, Encode and Decode any file to a hex string.  
+
+Compress/Decompress, Encode and Decode any file to a z85 string.
 Useful to store binaries in text format, for NFC cards/stickers/keychains or anywhere you want.
 
 ## Build
+
 ```bash
 #Build
 ❯ cargo build --release
 ```
 
 ## Usage
-Compress a file and encode bytes to hex
+
+Compress a file and encode bytes to z85
+
 ```bash
 #Create a test file
 echo "This is a test" >> raw.txt
@@ -19,7 +23,8 @@ echo "This is a test" >> raw.txt
 78da0bc9c82c5600a2448592d4e2122e0029730500
 ```
 
-Decode hex and decompress back to original file
+Decode z85 and decompress back to original file
+
 ```bash
 #Decode
 ❯ echo "78da0bc9c82c5600a2448592d4e2122e0029730500" | ./target/release/byteme out.txt
@@ -28,10 +33,10 @@ Decode hex and decompress back to original file
 This is a test
 ```
 
-
-
 ## Encryption
+
 Encrypt a file using `gpg` and process with `byteme`
+
 ```bash
 #Interactive mode
 ❯ gpg -c --no-symkey-cache raw.txt
@@ -41,8 +46,8 @@ Encrypt a file using `gpg` and process with `byteme`
 
 Now run the steps above but using `<filename>.gpg` instead.
 
-
 ## Decryption
+
 ```bash
 ❯ gpg -d raw.txt.gpg
 ```
--- src/main.rs
+++ src/main.rs
@@ -3,70 +3,76 @@ use core::str;
 use flate2::read::ZlibDecoder;
 use flate2::write::ZlibEncoder;
 use flate2::Compression;
+use itertools::Itertools;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::io::{Read, Write};
 use std::path::Path;
-use std::process;
-use std::{fmt::Write as fmtWrite, num::ParseIntError};
+use z85::*;
+
+const CHUNK_SIZE: usize = 2048;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let tty = atty::is(atty::Stream::Stdin);
     //Usage
-    if args.get(1).is_none() {
+    if let Some(arg) = args.get(1) {
+        match tty {
+            //Decode + Decompress + Save to file
+            false => from_raw(arg)?,
+            //Read from path + Compress + Encode
+            true => to_raw(arg)?,
+        };
+    } else {
         println!(
             "
-        Provided path will be compressed and a hex string will be sent stdout.
+        Provided file path will be compressed encoded to z85.
+        Result string will be sent stdout.
         To convert back to original file read instructions below.
         ----------------
         Usage:
         Compress: byteme <path>
-        Decompress: echo '<your-hex-string> | byteme <path-to-save-output-file>'
+        Decompress: echo '<your-z85-string> | byteme <output-path>'
         ---------------
         Hint: If you're seeing this message something went wrong.
         "
         );
-        process::exit(0x0100);
-    } else {
-        match tty {
-            false => {
-                //Decode + Decompress + Save to file
-                let mut buffer = String::new();
-                let stdin = io::stdin();
-                let mut handle = stdin.lock();
-                handle.read_line(&mut buffer)?;
-                write_to_file(decompress(decode_hex(buffer.trim_end())?)?, &args[1])?;
-            }
-            true => {
-                //Read from path + Compress + Encode
-                match Path::new(&args[1]).exists() {
-                    true => println!("{}", encode_hex(&compress(read_from_file(&args[1])?)?)),
-                    false => println!("File doesn't exist. Check path"),
-                };
-                process::exit(0x0100);
-            }
-        }
     };
 
     Ok(())
 }
-
-pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
-    (0..s.len())
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
-        .collect()
+fn from_raw(arg: &str) -> Result<()> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    handle.read_line(&mut buffer)?;
+    write_to_file(decompress(decode(buffer.trim_end())?)?, arg)?;
+    Ok(())
 }
 
-pub fn encode_hex(bytes: &[u8]) -> String {
-    let mut s = String::with_capacity(bytes.len() * 2);
-    for &b in bytes {
-        write!(&mut s, "{:02x}", b).unwrap();
-    }
-    s
+fn to_raw(arg: &str) -> Result<()> {
+    match Path::new(arg).exists() {
+        true => {
+            let bytes = encode(&compress(read_from_file(arg)?)?);
+            let result: String = str::from_utf8(
+                bytes
+                    .chars()
+                    .chunks(CHUNK_SIZE)
+                    .into_iter()
+                    .map(|x| x.collect())
+                    .collect::<Vec<String>>()
+                    .join("\n")
+                    .as_bytes(),
+            )?
+            .to_string();
+            println!("{}", result);
+        }
+        false => println!("File doesn't exist. Check path"),
+    };
+    Ok(())
 }
+
 pub fn write_to_file(data: Vec<u8>, path: &str) -> Result<()> {
     let mut file = File::create(path)?;
     file.write_all(&data).unwrap();
